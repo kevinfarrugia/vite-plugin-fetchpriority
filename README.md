2490a49c4ec6d3403efef57429b5d2e1ed12c0e4
Kevin Farrugia <hello@imkev.dev>
2024-03-26 10:43:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# vite-plugin-fetchpriority
+
+Vite plugin to add the `fetchpriority` attribute to scripts, stylesheets, and link rel="preload".
+
+## Installation
+
+```bash
+# npm
+npm install vite-plugin-fetchpriority -D
+
+# yarn
+yarn add vite-plugin-fetchpriority -D
+```
+
+## Usage
+
+```js
+// vite.config.js
+import fetchPriority from "vite-plugin-fetchpriority";
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'output',
+    format: 'cjs'
+  },
+  fetchPriority([
+    // add fetchpriority="high" to app.js
+    {
+      priority: "high",
+      files: [
+        {
+          type: "script",
+          match: /app\.js$/
+        }
+      ]
+    },
+    // add fetchpriority="high" to all preloads
+    {
+      priority: "high",
+      files: [
+        {
+          type: "preload"
+        }
+      ]
+    }
+  ])
+};
+```
+
+The output of the above configuration will be:
+
+```diff
+<head>
+  <title>Demo</title>
+  <link rel="stylesheet" href="/app.css" />
++  <script type="module" src="/app.js" fetchpriority="high"></script>
++  <link rel="preload" href="/roboto.woff2" type="font/woff2" as="font" crossorigin="anonymous" fetchpriority="high" />
++  <link rel="preload" href="/roboto-semibold.woff2" type="font/woff2" as="font" crossorigin="anonymous" fetchpriority="high" />
+</head>
+```
+
+### Options
+
+#### `priority`
+
+Type: `high|low`
+
+The value of the `fetchpriority` attribute. Note that the plugin does not support `auto` as this is the default value and can be omitted.
+
+#### `files`
+
+Type: `Array<{ type: string, match?: RegExp }>`
+
+The files to add the `fetchpriority` attribute to. The `type` is used to determine the tag to add the attribute to (`script`, `link rel="stylesheet"`, or `link rel="preload"`). The `match` is optionally used to determine which files to add the attribute to if it isn't required on all files.
+
+## Motivation
+
+The Fetch Priority API can be useful for improving the performance of websites by giving the browser hints on which resources to prioritize. This plugin allows you to add the `fetchpriority` attribute to resources created in your Vite build and not accessible in the HTML.
+
+Read more:
+- [Optimizing resource loading with the Fetch Priority API](https://web.dev/articles/fetch-priority)
+- [Fetch Priority and optimizing LCP](https://imkev.dev/fetchpriority-opportunity)
+
+## License
+
+[Apache-2.0](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+import type { Plugin } from "vite";
+
+interface FetchPriorityOptionsFiles {
+  /**
+   * HTML element type
+   */
+  type: "script" | "style" | "preload";
+  /**
+   * Regular expression to target files
+   */
+  match?: RegExp;
+}
+
+type FetchPriorityOptions = {
+  priority: "high" | "low";
+  files: FetchPriorityOptionsFiles[];
+};
+
+const fetchPriority = (options: FetchPriorityOptions[]): Plugin => ({
+  name: "fetchPriority",
+  transformIndexHtml: {
+    order: "post",
+    handler(html) {
+      if (!options) return html;
+
+      let formattedHtml = html;
+
+      options.forEach((option) => {
+        option.files.forEach((file) => {
+          if (file.type === "script") {
+            // external scripts
+            const scriptRegex =
+              /<script[^>]*(?=.*src=["'].+["'])[^>]*>.*<\/script>/gm;
+            const tags = html.match(scriptRegex);
+
+            if (tags) {
+              formattedHtml = formattedHtml.replace(scriptRegex, (tag) => {
+                // if tag does not have src attribute, return the tag as is
+                const srcMatch = tag.match(/src=["'].+["']/);
+                if (!srcMatch) return tag;
+
+                // if tag has a src attribute and it does not match the user's regex, return the tag as is
+                if (
+                  srcMatch &&
+                  file.match &&
+                  !file.match.test(srcMatch.join(""))
+                ) {
+                  return tag;
+                }
+
+                // replace each tag with itself and add the fetchpriority attribute
+                return tag.replace(
+                  "></script>",
+                  ` fetchpriority="${option.priority}"></script>`
+                );
+              });
+            }
+          } else if (file.type === "style") {
+            // external stylesheets
+            const styleRegex =
+              /<link[^>]*(?=.*rel="stylesheet")(?=.*href=["'].+["'])[^>]*>/gm;
+            const tags = html.match(styleRegex);
+
+            if (tags) {
+              // replace each tag with itself and add the fetchpriority attribute
+              formattedHtml = formattedHtml.replace(styleRegex, (tag) => {
+                // if tag does not have href attribute, return the tag as is
+                const hrefMatch = tag.match(/href=["'].+["']/);
+                if (!hrefMatch) return tag;
+
+                // if tag has a href attribute and it does not match the user's regex, return the tag as is
+                if (
+                  hrefMatch &&
+                  file.match &&
+                  !file.match.test(hrefMatch.join(""))
+                ) {
+                  return tag;
+                }
+
+                // replace each tag with itself and add the fetchpriority attribute
+                return tag.replace(">", ` fetchpriority="${option.priority}">`);
+              });
+            }
+          } else if (file.type === "preload") {
+            // link rel="preload"
+            const preloadRegex =
+              /<link[^>]*(?=.*rel="preload")(?=.*href=["'].+["'])[^>]*>/gm;
+            const tags = html.match(preloadRegex);
+
+            if (tags) {
+              formattedHtml = formattedHtml.replace(preloadRegex, (tag) => {
+                // if tag does not have href attribute, return the tag as is
+                const hrefMatch = tag.match(/href=["'].+["']/);
+                if (!hrefMatch) return tag;
+
+                // if tag has a href attribute and it does not match the user's regex, return the tag as is
+                if (
+                  hrefMatch &&
+                  file.match &&
+                  !file.match.test(hrefMatch.join(""))
+                ) {
+                  return tag;
+                }
+
+                // replace each tag with itself and add the fetchpriority attribute
+                return tag.replace(">", ` fetchpriority="${option.priority}">`);
+              });
+            }
+          }
+        });
+      });
+
+      return formattedHtml;
+    },
+  },
+});
+
+export default fetchPriority;
